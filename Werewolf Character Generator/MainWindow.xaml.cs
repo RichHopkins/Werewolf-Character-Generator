623d8fe3cfe2e189419f9e2de06e44a7b0dd748a
Richard Hopkins <rhopkins@neogov.net>
2021-03-10 16:04:34 -0800

    Add project files.

---
+++ Werewolf Character Generator/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Werewolf_Character_Generator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Str_ValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+
+        }
+
+        private void Dex_ValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+
+        }
+
+        private void Sta_ValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+
+        }
+
+        private void Cha_ValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+
+        }
+
+        private void Man_ValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+
+        }
+
+        private void App_ValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+
+        }
+
+        private void Per_ValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+
+        }
+
+        private void Int_ValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+
+        }
+
+        private void Wit_ValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+
+        }
+
+        private void Auspice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Breed_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Tribe_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CharacterName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PlayerName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Chronicle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PackName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PackTotem_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Concept_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Werewolf Character Generator/werewolf.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Werewolf_Character_Generator
+{
+    public class Werewolf
+    {
+        public string CharacterName;
+        public string PlayerName;
+        public string Chronicle;
+        public string Breed;
+        public string Auspice;
+        public string Tribe;
+        public string PackName;
+        public string PackTotem;
+        public string Concept;
+        public byte Strength;
+        public byte Dexterity;
+        public byte Stamina;
+        public byte Charisma;
+        public byte Manipulation;
+        public byte Appearance;
+        public byte Perception;
+        public byte Intelligence;
+        public byte Wits;
+    }
+}
