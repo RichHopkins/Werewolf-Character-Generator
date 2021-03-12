af7dd03b1ea788d785a1d467c3f78667ef1a53f3
Richard Hopkins <rhopkins@neogov.net>
2021-03-12 09:45:19 -0800

    .

--- Werewolf Character Generator/MainWindow.xaml.cs
+++ Werewolf Character Generator/MainWindow.xaml.cs
@@ -31,11 +31,6 @@ namespace Werewolf_Character_Generator
             //Werewolf werewolf = new Werewolf();
         }
 
-        public static void PrintToPDF() 
-        {
-            
-        }
-
         public static void OpenXMLFile()
         {
             
@@ -44,6 +39,108 @@ namespace Werewolf_Character_Generator
         public static void SaveXMLFile()
         {
 
+        }
+        public static void PrintToPDF()
+        {
+
+        }
+
+        private void NewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ClearAllFields();
+        }
+
+        private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenXMLFile();
+        }
+
+        private void SaveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SaveXMLFile();
+        }
+
+        private void PrintMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            PrintToPDF();
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Auspice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem)Auspice.SelectedItem;
+            string content = item.Content.ToString();
+            werewolf.Auspice = content;
+
+        }
+
+        private void Breed_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem)Breed.SelectedItem;
+            string content = item.Content.ToString();
+            werewolf.Breed = content;
+        }
+
+        private void Tribe_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem)Tribe.SelectedItem;
+            string content = item.Content.ToString();
+            werewolf.Tribe = content;
+        }
+
+        private void CharacterName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            werewolf.CharacterName = CharacterName.Text;
+        }
+
+        private void PlayerName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            werewolf.PlayerName = PlayerName.Text;
+        }
+
+        private void Chronicle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            werewolf.Chronicle = Chronicle.Text;
+        }
+
+        private void PackName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            werewolf.PackName = PackName.Text;
+        }
+
+        private void PackTotem_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            werewolf.PackTotem = PackTotem.Text;
+        }
+
+        private void Concept_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            werewolf.Concept = Concept.Text;
+        }
+
+        private void Physical_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem)Physical.SelectedItem;
+            string content = item.Content.ToString();
+            werewolf.Physical = content;
+        }
+
+        private void Social_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem)Social.SelectedItem;
+            string content = item.Content.ToString();
+            werewolf.Social = content;
+        }
+
+        private void Mental_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem)Mental.SelectedItem;
+            string content = item.Content.ToString();
+            werewolf.Mental = content;
         }
 
         private void Str_ValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
@@ -89,78 +186,6 @@ namespace Werewolf_Character_Generator
         private void Wit_ValueChanged(object sender, RoutedPropertyChangedEventArgs<int> e)
         {
             werewolf.Wits = WitRating.Value;
-        }
-
-        private void Auspice_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBoxItem item = (ComboBoxItem)Auspice.SelectedItem;
-            werewolf.Auspice = item.Content.ToString();
-
-        }
-
-        private void Breed_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBoxItem item = (ComboBoxItem)Breed.SelectedItem;
-            werewolf.Breed = item.Content.ToString();
-        }
-
-        private void Tribe_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBoxItem item = (ComboBoxItem)Tribe.SelectedItem;
-            werewolf.Tribe = item.Content.ToString();
-        }
-
-        private void CharacterName_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            werewolf.CharacterName = CharacterName.Text;
-        }
-
-        private void PlayerName_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            werewolf.PlayerName = PlayerName.Text;
-        }
-
-        private void Chronicle_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            werewolf.Chronicle = Chronicle.Text; 
-        }
-    
-        private void PackName_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            werewolf.PackName = PackName.Text;
-        }
-
-        private void PackTotem_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            werewolf.PackTotem = PackTotem.Text;
-        }
-
-        private void Concept_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            werewolf.Concept = Concept.Text;
-        }
-
-        private void NewMenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            ClearAllFields();
-        }
-
-        private void OpenMenuItem_Click(object sender, RoutedEventArgs e) 
-        {
-            OpenXMLFile();
-        }
-
-        private void SaveMenuItem_Click(object sender, RoutedEventArgs e) 
-        {
-            SaveXMLFile();
-        }
-
-        private void PrintMenuItem_Click(object sender, RoutedEventArgs e) {
-            PrintToPDF();
-        }
-
-        private void ExitMenuItem_Click(object sender, RoutedEventArgs e) {
-            Application.Current.Shutdown();
         }
     }
 }
--- Werewolf Character Generator/werewolf.cs
+++ Werewolf Character Generator/werewolf.cs
@@ -17,38 +17,20 @@ namespace Werewolf_Character_Generator
         public string PackName;
         public string PackTotem;
         public string Concept;
+        public string Physical;
         public int Strength;
         public int Dexterity;
         public int Stamina;
+        public string Social;
         public int Charisma;
         public int Manipulation;
         public int Appearance;
+        public string Mental;
         public int Perception;
         public int Intelligence;
         public int Wits;
 
         public Werewolf() {
-        }
-
-        public Werewolf(string characterName, string playerName, string chronicle, string breed, string auspice, string tribe, string packName, string packTotem, string concept, int strength, int dexterity, int stamina, int charisma, int manipulation, int appearance, int perception, int intelligence, int wits) {
-            CharacterName = characterName;
-            PlayerName = playerName;
-            Chronicle = chronicle;
-            Breed = breed;
-            Auspice = auspice;
-            Tribe = tribe;
-            PackName = packName;
-            PackTotem = packTotem;
-            Concept = concept;
-            Strength = strength;
-            Dexterity = dexterity;
-            Stamina = stamina;
-            Charisma = charisma;
-            Manipulation = manipulation;
-            Appearance = appearance;
-            Perception = perception;
-            Intelligence = intelligence;
-            Wits = wits;
         }
     }
 }
